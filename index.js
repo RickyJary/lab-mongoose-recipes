465435013e1ad385ac1049ebc6e1e7525c7ee4df
RickJary <ricardogarciajarillo0209@gmail.com>
2024-11-07 00:52:47 +0100

    all iterations done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require('./data.json');
 
 const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
@@ -12,12 +12,50 @@ mongoose
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: "RTest Recipe",
+      level: "Easy Peasy",
+      ingredients: [
+        "2 pounds red onions, sliced salt to taste",
+        "2 (16 ounce) boxes uncooked rigatoni",
+        "1 tablespoon chopped fresh marjoram leaves",
+        "1 pinch cayenne pepper",
+        "2 tablespoons freshly grated Parmigiano-Reggiano cheese"
+      ],
+      cuisine: "Italian",
+      dishType: "main_course",
+      image: "https://images.media-allrecipes.com/userphotos/720x405/3489951.jpg",
+      duration: 220,
+      creator: "Chef Luigi"
+    });
+  })
+  .then(() => {
+    return Recipe.insertMany(data);
+  })
+  .then(recipesCreated => {
+    console.log('Recipes created:', recipesCreated);
+    return Recipe.findOneAndUpdate(
+      {title: "Rigatoni alla Genovese"},
+      {duration: 100},
+      {new: true}
+    )
+  })
+  .then(() => {
+    return Recipe.deleteOne(
+      {title: "Carrot Cake"}
+    )
+  })
+  .then((deletedRecipe) => {
+    console.log(deletedRecipe)
+    return mongoose.connection.close(); 
+  })
+  .then(() => {
+    console.log('Connection closed');
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
